c4b0ab1006626c7f556f0fecf14c390348501f75
tofcap2 <cap2family@hotmail.fr>
2018-11-20 15:14:18 +0100

    Creation d'entite et ...

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,19 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\DataFixtures\AuthorFixtures;
+use App\Entity\Author;
+use App\Entity\Category;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends AbstractController
+class DefaultController extends BaseController
 {
     /**
-     * @Route("/default", name="default")
+     * @Route("/default/{nom}", name="default")
      */
-    public function index()
+    public function index(string $nom)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/DefaultController.php',
-        ]);
+        $author = $this->getDoctrine()->getRepository(Author::class)->findOneBy(["lastname" => $nom]);
+
+        return new Response($author->getFirstname() . " " . $author->getLastname());
     }
 }
