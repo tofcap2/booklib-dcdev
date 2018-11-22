8f9564d0e8b8e42f0dac7243834bfe3d60d22c1f
tofcap2 <cap2family@hotmail.fr>
2018-11-22 17:02:07 +0100

    Création formulaire ajout auteur

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -9,7 +9,42 @@
 namespace App\Controller;
 
 
-class AuthorController
-{
+use App\Entity\Author;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class AuthorController
+ * @package App\Controller
+ * @Route("/author")
+ */
+class AuthorController extends BaseController
+{
+    /**
+     * @Route("/new", name="author_new")
+     */
+    public function new(Request $request)
+    {
+        $author = new Author();
+
+        $form = $this->createFormBuilder($author)
+                    ->add('firstname', TextType::class)
+                    ->add('lastname', TextType::class)
+                    ->add('save', SubmitType::class)
+                    ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($author);
+            $em->flush();
+
+            return $this->render('author/new.html.twig', ['form' => $form->createView()]);
+        }
+
+        return $this->render('author/new.html.twig',['form' => $form->createView()]);
+    }
 }
